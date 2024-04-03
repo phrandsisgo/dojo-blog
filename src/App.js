261c692c349dc85dbd0a9639a4a38d152848ab9b
Francisco W <francisco.wohlgemuth@hotmail.com>
2024-04-03 13:40:50 +0200

    updated up to video #5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
 
 function App() {
   const title ="Welcome to the new blog";
   const likes = 50;
+  const linguatechLink ="https://linguatech.ch/"
   return (
     <div className="App">
+        <Navbar />
         <div className="content">
           <h1>App Component</h1>
-          <p>This is the content of the app component</p>
+          <h2>{title}</h2>
+          <p>This App has been liked {likes} times</p>
         </div>
+        <a href={linguatechLink}>linguatech</a>
+        <br />
         <a
           className="App-link"
           href="https://reactjs.org"
