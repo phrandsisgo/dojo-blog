379f874f1175404bace0588aa4ec5553e1acfe86
Francisco W <francisco.wohlgemuth@hotmail.com>
2024-04-04 13:56:53 +0200

    function calling on video #8

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,19 @@
 const Home = () => {
-    return ( 
-        <h2>homepage</h2>
+    const handleClick = (e) => {
+        console.log('hello, luis', e);
+    }
+    const handleOneMoreTime = (name) => {
+        console.log(name)
+    }
+    return (
+    <div>
+        <h2 style={{color:'blue'}}>Homepage</h2>
+        <button onClick={handleClick}>click me</button>
+        <button onClick={()=>{
+         handleOneMoreTime('luis')   
+        }} >click on luis</button>
+       
+    </div>
      );
 }
  
