443d591df5ce53a25f10a978a3062a693ece7800
Francisco W <francisco.wohlgemuth@hotmail.com>
2024-04-11 01:53:20 +0200

    fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -4,27 +4,24 @@ import BlogList from './BlogList';
 //const Home is a stateless functional component
 const Home = () => {
     //define a state variable called blogs and a function to update it called setBlogs
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState('mario');
     
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(()=>{
-        console.log('useEffect ran');
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, []);
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Marios Blogs" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
             <button onClick={()=>setName('luigi')}>change name</button>
             <p>{name}</p>
             <h2 style={{color:'blue'}}>Homepage</h2>
