d28b9c3f7e8fea7012b62b6befb64928b9d7da19
Francisco W <francisco.wohlgemuth@hotmail.com>
2024-04-18 12:07:04 +0200

    #20 making custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,16 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 //const Home is a stateless functional component
 const Home = () => {
-    //define a state variable called blogs and a function to update it called setBlogs
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    const [name, setName] = useState('mario');
-    
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    console.log(res);
-                    return res.json();
-                
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
-                    //console.log(err.message);
-                });
-        }, 1000);
-    }, []);
+   const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     return (
         <div className="home">
             {isPending && <div>Loading...</div>}
             {error && <div>{error}</div>}
+            
             {blogs && <BlogList blogs={blogs} title="All Blogs!!"/>}
-            <button onClick={()=>setName('luigi')}>change name</button>
-            <p>{name}</p>
             <h2 style={{color:'blue'}}>Homepage</h2>
         
         </div>
