8d8b54d03ea8cf2b2f4d3bef94ef85958308e19e
Francisco W <francisco.wohlgemuth@hotmail.com>
2024-04-18 13:17:14 +0200

    routing has been added #21

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
 
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+
 
 function App() {
   const title ="Welcome to the new blog";
   const likes = 50;
   const linguatechLink ="https://linguatech.ch/"
   return (
-    <div className="App">
-        <Navbar />
-        <div className="content">
-          <Home />
-        </div>
-        <a href={linguatechLink}>linguatech</a>
-        <br />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
+    <Router>
+      <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+          <a href={linguatechLink}>linguatech</a>
+
+      </div>
+    </Router>
   );
 }
 
